142a2b0dc87624e3248bc47f1c495d08e750c57a
Nadia <nsara1112@gmail.com>
2023-05-03 01:20:59 -0400

    Added comment

--- src/test/java/testNG_Maven/TestNgMavenExampleTest.java
+++ src/test/java/testNG_Maven/TestNgMavenExampleTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 public class TestNgMavenExampleTest {
     private static final Logger LOGGER = LogManager.getLogger(TestNgMavenExampleTest.class);
 
+    //This is the data provider method
     @Test(dataProvider = "MultipleColumnValues", dataProviderClass = DataStore.class)
     public void run(String name,String state, int zipCode) {
         LOGGER.info("Name is: " +name);
